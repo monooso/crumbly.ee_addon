2d504b9dca3341b76ad57b126c60811cdcbb0570
Stephen Lewis <stephen@experienceinternet.co.uk>
2012-03-14 10:00:01 +0000

    Create NSM Addon Updater config file.

---
+++ third_party/crumbly/config.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Crumbly NSM Add-on Updater information.
+ *
+ * @author          Stephen Lewis (http://github.com/experience/)
+ * @copyright       Experience Internet
+ * @package         Crumbly
+ */
+
+if ( ! defined('CAMPAIGNER_NAME'))
+{
+  define('CRUMBLY_NAME', 'Crumbly');
+  define('CRUMBLY_VERSION', '1.1.0');
+}
+
+$config['name']     = CRUMBLY_NAME;
+$config['version']  = CRUMBLY_VERSION;
+$config['nsm_addon_updater']['versions_xml']
+  = 'http://experienceinternet.co.uk/software/feeds/crumbly';
+
+/* End of file      : config.php */
+/* File location    : third_party/crumbly/config.php */
