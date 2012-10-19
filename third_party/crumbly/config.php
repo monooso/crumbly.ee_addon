b1f61244b18c95ebd5dd11ce9e63e2e5e4495dbc
Stephen Lewis <stephen@experienceinternet.co.uk>
2012-10-19 19:23:57 +0100

    Bump version.

--- third_party/crumbly/config.php
+++ third_party/crumbly/config.php
@@ -12,7 +12,7 @@
 if ( ! defined('CRUMBLY_NAME'))
 {
   define('CRUMBLY_NAME', 'Crumbly');
-  define('CRUMBLY_VERSION', '1.2.1');
+  define('CRUMBLY_VERSION', '1.2.2');
 }
 
 $config['name']     = CRUMBLY_NAME;
